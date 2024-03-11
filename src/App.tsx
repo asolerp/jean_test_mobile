329d09cd67d1586b847ba6c96b8482e084d9bcc6
Alberto Soler <albertosolpal@gmail.com>
2024-03-11 17:13:07 +0100

    feat: added splashscreen android and ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Config from 'react-native-config'
 
 import { ApiProvider } from '@state/context/ApiContext'
 import { RootNavigation } from '@navigation/RootNavigation'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <ApiProvider url={String(Config.API_URL)} token={String(Config.API_TOKEN)}>
       <RootNavigation />
--- src/navigation/utils/types.ts
+++ src/navigation/utils/types.ts
@@ -4,6 +4,10 @@ import { MAIN_STACK_KEY } from './keys'
 export type RoutingData<T> = { routingData: T }
 export type StackParams<T> = { params: T }
 
+export type RootStackParamList = {
+  [MAIN_STACK_KEY]: any
+}
+
 //@TODO: move to Main.tsx stack after migration
 export type MainStackParamList = {
   [DUMMY_SCREEN_KEY]: any
