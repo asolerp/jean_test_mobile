833184f65180eff259a41724b627e74bd030ab39
Alberto Soler <albertosolpal@gmail.com>
2024-03-13 15:41:32 +0100

    feat: created skeleton for invoices list

---
+++ src/components/features/HomeScreen/InvoicesPlaceholder.tsx
@@ -0,0 +1,26 @@
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder'
+
+export const InvoicesPlaceholder = () => {
+  const skeletons = Array.from({ length: 6 }, (_, index) => (
+    <SkeletonPlaceholder key={index} borderRadius={3}>
+      <SkeletonPlaceholder.Item
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        marginTop={20}
+      >
+        <SkeletonPlaceholder.Item>
+          <SkeletonPlaceholder.Item width={80} height={10} />
+          <SkeletonPlaceholder.Item marginTop={6} width={120} height={20} />
+          <SkeletonPlaceholder.Item marginTop={6} width={170} height={10} />
+        </SkeletonPlaceholder.Item>
+        <SkeletonPlaceholder.Item alignItems="flex-end">
+          <SkeletonPlaceholder.Item width={50} height={15} />
+          <SkeletonPlaceholder.Item marginTop={6} width={80} height={20} />
+        </SkeletonPlaceholder.Item>
+      </SkeletonPlaceholder.Item>
+    </SkeletonPlaceholder>
+  ))
+
+  return <>{skeletons}</>
+}
