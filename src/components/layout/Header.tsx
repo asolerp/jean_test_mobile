e4bc1c71abdec2a8fa1db1887144630e613c5d6c
Alberto Soler <albertosolpal@gmail.com>
2024-03-13 12:31:05 +0100

    feat: added more params to header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,17 +4,32 @@ import { popScreen } from '@src/navigation/utils/actions'
 import { GRAY_900 } from '@src/utils/colors'
 
 type HeaderProps = {
-  title: string
+  title?: string
+  mode?: Mode.WHITE | Mode.DARK
+  rightComponent?: React.ReactNode
 }
 
-export const Header: React.FC<HeaderProps> = ({ title }) => {
+export enum Mode {
+  WHITE = 'white',
+  DARK = 'dark',
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  title,
+  mode = Mode.DARK,
+  rightComponent,
+}) => {
   return (
     <View className="flex-row items-center p-6 justify-between">
       <TouchableOpacity onPress={() => popScreen()}>
-        <ArrowLeft width={30} height={30} fill={GRAY_900} />
+        <ArrowLeft
+          width={30}
+          height={30}
+          fill={mode === Mode.DARK ? GRAY_900 : 'white'}
+        />
       </TouchableOpacity>
       <Text className="font-bol text-gray-900 text-2xl">{title}</Text>
-      <View className="w-5" />
+      {rightComponent ? rightComponent : <View className="w-5" />}
     </View>
   )
 }
